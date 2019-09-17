4356ed97371e86b2741262f446a3c3941818f8b9
Rodolpho Nazareth Rossete De Souza <rnsouza1@stefanini.com>
2019-09-17 10:55:32 -0300

    Criação do Gateway e do serviço de registro dos microservices

--- courses/src/main/java/aulas/devdojo/micosservice/course/CourseApplication.java
+++ courses/src/main/java/aulas/devdojo/micosservice/course/CourseApplication.java
@@ -3,11 +3,13 @@ package aulas.devdojo.micosservice.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan({"course.microservice.core.model"})
 @EnableJpaRepositories({"course.microservice.core.repository"})
+@EnableEurekaClient
 public class CourseApplication {
 
     public static void main(String[] args) {
